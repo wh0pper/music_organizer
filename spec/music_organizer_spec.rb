1e5dc4de6b7b9d9896fb201d06a503e8a2e0997e
Jared Clemmensen and Masen Matthews <jared@>
2018-02-15 09:40:20 -0800

    passes initialize test

--- spec/music_organizer_spec.rb
+++ spec/music_organizer_spec.rb
@@ -3,12 +3,10 @@ require('artist')
 require('pry')
 
 describe('Artist') do
-  before do
-    artist = Artist.new({'name' => 'Mewwithoutyou', 'genre' => 'rock'})
-  end
 
   describe('#initalize') do
     it('initializes a new artist object') do
+      artist = Artist.new({:name => 'Mewwithoutyou', :genre => 'rock'})
       expect(artist.name).to(eq('Mewwithoutyou'))
       expect(artist.genre).to(eq('rock'))
     end
