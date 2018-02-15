f0999045f9e518110a6cd019499ed6d4a5fbb397
Jared Clemmensen and Masen Matthews <masen@>
2018-02-15 11:56:40 -0800

    fix list display and add clear button

--- app.rb
+++ app.rb
@@ -1,16 +1,25 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
-require('./lib/favorite_cities')
+require('./lib/artist')
+# require('./lib/album')
 require('rspec')
 require('pry')
 
 get('/') do
+  @list = Artist.list
+  erb(:artist_input)
+end
 
-  erb(:input)
+post('/clear') do
+  Artist.clear
+  redirect '/'
 end
 
 post('/') do
-
-  erb(:input)
+  artist = params["artist"]
+  artist_val = Artist.new({:name => artist})
+  artist_val.add_to_list()
+  @list = Artist.list
+  erb(:artist_input)
 end
