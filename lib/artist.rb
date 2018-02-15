c511e82f31f87aa49026efebfd5204b32a26c347
Jared Clemmensen and Masen Matthews <masen@>
2018-02-15 10:45:48 -0800

    incorporate album object into artist and add passing test

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,13 @@
 #!/usr/bin/ruby
 
 class Artist
-  attr_reader :name, :genre, :list
+  attr_reader :name, :genre, :list, :albums
   @@list = []
 
   def initialize(attributes)
     @name = attributes[:name]
     @genre = attributes[:genre]
+    @albums = []
   end
 
   def add_to_list
@@ -15,6 +16,12 @@ class Artist
 
   def self.list
     @@list
+  end
+
+  def add_album(attributes)
+    new_album = Album.new(attributes)
+    self.albums.push(new_album)
+    new_album
   end
 
 end
--- spec/music_organizer_spec.rb
+++ spec/music_organizer_spec.rb
@@ -20,13 +20,22 @@ describe('Artist') do
       expect(Artist.list()).to(eq([artist]))
     end
   end
+
+  describe('#add_album') do
+    it('adds album object to albums array within artist object') do
+      artist = Artist.new({:name => 'Mewithoutyou', :genre => 'rock'})
+      this_album = artist.add_album({:album_name => "AB Life", :year => "2002"})
+      expect(artist.albums).to(eq([this_album]))
+    end
+  end
 end
 
-  describe('Album') do
-    describe('#initialize')
+describe('Album') do
+  describe('#initialize') do
     it('Initializes a new album object') do
       album = Album.new({:album_name => "AB Life", :year => "2002"})
       album.add_to_list
       expect(Album.list()).to(eq([album]))
     end
   end
+end
