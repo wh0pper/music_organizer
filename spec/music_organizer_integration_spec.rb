65fb1f3525df5bd0026f8b5f40c49f1bc6cdaa85
Jared Clemmensen and Masen Matthews <jared@>
2018-02-15 16:04:51 -0800

    add integration spec

--- spec/music_organizer_integration_spec.rb
+++ spec/music_organizer_integration_spec.rb
@@ -11,3 +11,12 @@ describe('button adds artists to list', {:type => :feature}) do
     expect(page).to have_content('Radiohead')
   end
 end
+
+describe('clear button removes artist inputs from field', {:type => :feature}) do
+  it('removes all user inputs from list') do
+    visit('/')
+    fill_in('artist', :with => 'Radiohead')
+    click_button('clear')
+    expect(page.has_content?('Radiohead')).to(eq(false)) 
+  end
+end
